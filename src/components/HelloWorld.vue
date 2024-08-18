0f6b5a5da52295a99cd83ef94071b62b6534fb99
Rehanullah Khan <reh1hat@gmail.com>
2024-08-18 11:47:20 -0700

    conflict

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
     </p>
     <h3>Installed CLI Plugins </h3>
     <p>this is tmp branch para</p>
+    <p>this is tmp branch para 2</p>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
